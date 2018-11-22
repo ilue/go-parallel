26443bbb2abb5b351077aef890fdf21164392bca
ilue <ilue26@gmail.com>
2018-11-22 16:53:54 +0800

    Create parallel.go

---
+++ parallel.go
@@ -0,0 +1,31 @@
+package parallel
+
+import (
+	"runtime"
+	"sync"
+
+	"github.com/dgrr/GoSlaves"
+)
+
+type parallelForBody struct {
+	wg  *sync.WaitGroup
+	f   func(int)
+	arg int
+}
+
+var parallelForPool = slaves.NewPool(runtime.NumCPU()*2, func(obj interface{}) {
+	body := obj.(*parallelForBody)
+	defer body.wg.Done()
+	body.f(body.arg)
+})
+
+func ParallelFor(first, last int, f func(int)) {
+	var wg sync.WaitGroup
+	wg.Add(last - first)
+
+	for i := first; i < last; i++ {
+		parallelForPool.Serve(&ParallelForBody{&wg, f, i})
+	}
+
+	wg.Wait()
+}
